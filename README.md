4b4c1d1e6d74e58d6c74333f773fc5b34d1c5d96
Alesso  Ferrari <orangeferrari212@gmail.com>
2022-09-27 11:58:24 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+![jeopardy 1 ](https://user-images.githubusercontent.com/99289954/192410614-f4616220-f345-4092-b3cc-de51f8c91d3b.png)
+![JEOPARDY2](https://user-images.githubusercontent.com/99289954/192410630-a8b77f80-1d39-4b42-80fc-10f3e5ad88a8.png)
+![JEOPARDY 3 RED](https://user-images.githubusercontent.com/99289954/192410644-9ebda1ae-9aee-4e8c-b343-aa37ed9f67ea.png)
+![jeopardy 4 score 1](https://user-images.githubusercontent.com/99289954/192410648-63de90d1-082b-439e-8799-d78adc8d26d4.png)
---
+++ code.js
@@ -0,0 +1,191 @@
+
+
+// Thank you W3 SCHOOLS HELPING TO DECODE HUE HTML5 WITH COLORS //
+// GITHUB ALWAYS THERE FOR CODES AND IDEAS 
+
+
+
+let cluesArray = [];
+let questionNumber = 0;
+let score = 0
+let exhaustButton = document.getElementById("exhaust-btn");
+let exhaustButtonH = document.getElementById("exhaustH-btn");
+let questionAnswer = document.getElementById("questionAnswer");
+let answerInput = document.getElementById("userAnswer");
+let resetBtnAnswer = document.getElementById("reset");
+let scoreMark = document.getElementById("scoreMark");
+let answerCheck = document.getElementById("checkAnswer");
+let questionText = document.getElementById("question");
+let matchContainer = document.querySelector('.container');
+let greeted = document.getElementById("Jeopardy" );
+let startingMinutes = 10;
+let time = startingMinutes *60;
+let countDownEl = document.getElementById("counterDown")
+let started = false;
+let interval = null
+
+exhaustButton.addEventListener("click", startGame)
+exhaustButtonH.addEventListener("click", startGame)
+function getData(){
+    fetch('https://jservice.kenzie.academy/api/random-clue?valid=true')
+    .then(response => response.json())
+    .then(data => data.categoryId)
+    .then(categoryId => {
+
+    fetch(`https://jservice.kenzie.academy/api/clues?category=${categoryId}`)
+    .then(response => response.json())
+    .then(data => {
+            cluesArray = data.clues
+            index = Math.floor((Math.random() * cluesArray.length - 1) + 1) 
+            currentClue = cluesArray.splice(index, 1)[0]
+            renderQuestion(currentClue)
+            return currentClue
+        })
+
+ function renderQuestion(jeopardy) {
+        console.log("Correct Answer:", jeopardy.answer)
+           document.getElementById('question').innerHTML = jeopardy.question
+           document.getElementById('category').innerHTML = jeopardy.category.title
+           
+        }
+    })
+}
+
+
+resetBtnAnswer.addEventListener("click", function(event) {
+
+    event.preventDefault();
+    let userAnswer = answerInput.value;
+    matchContainer.style.background = "red";
+    if (userAnswer === "") {
+        answerCheck.innerHTML = "";
+          setTimeout(function removeMessage() {
+        answerCheck.innerHTML = "ANSWER ME ANSWER ME ANSWER ME ANSWER ANSWER ME ANSWER ME ANS"; 
+    }, 4000) 
+
+    } else if (userAnswer === answerInput.value) {
+        score += 1;
+        scoreMark.innerHTML = "  🥇" + score; 
+        answerInput.value = "";
+        matchContainer.style.background = "Black";
+        answerCheck.innerHTML = " 5️⃣0️⃣0️⃣";
+    } 
+
+    else {
+
+        endGame()
+        
+    }
+
+    if (score === 5) {
+        console.log("Game Over !")
+        scoreMark.innerHTML = "";
+        questionText.innerHTML = "";
+        matchContainer.style.background = "Black";
+        exhaustButton.innerText = "💢🔟0️⃣0️⃣0️⃣";
+        exhaustButtonH.innerText = "💢 5️⃣0️⃣0️⃣0️⃣";
+        resetBtnAnswer.style.display = "none";
+        answerInput.style.display = "none"
+        answerCheck.innerHTML = "🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮"+
+         "🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🎮 🆗 ❌ 🎮 🆗 ❌ " 
+        + score;
+        category ="";
+        score = 0;
+    }
+    
+})
+
+function timer(){
+
+    let minutes = Math.floor(time/60);
+    let seconds = time% 60;
+    seconds = seconds< 10? "0" + seconds: seconds;
+    countDownEl .innerHTML = "" + " " +  `${minutes}: ${seconds}`;
+   time--;
+    if (time==0){
+        clearInterval (interval)
+        started =false;
+        countDownEl .innerHTML = "TIME OUT" 
+        time;
+   endGame()
+   if (score === 5) {
+    console.log("Game Over !")
+    scoreMark.innerHTML = "";
+    questionText.innerHTML = "";
+    matchContainer.style.background = "Black";
+    exhaustButton.innerText = "💢🔟0️⃣0️⃣0️⃣";
+    exhaustButtonH.innerText = " 💢5️⃣0️⃣0️⃣0️⃣";
+    resetBtnAnswer.style.display = "none";
+    answerInput.style.display = "none"
+    answerCheck.innerHTML = "🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕"+
+    " ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ "  
+     + score;
+    score = 0;
+    
+}
+
+    }
+ }
+
+function startGame() {
+
+    if(!started){
+
+    if(time >= 0){
+     interval= setInterval(timer, 1000); 
+        started = true;
+        time;
+    }
+
+    else{
+        clearInterval (interval)
+         started =false;
+        countDownEl .innerHTML = "TIME OUT" 
+   endGame()
+    console.log("Game Over !")
+    scoreMark.innerHTML = "";
+    questionText.innerHTML = "";
+    matchContainer.style.background = "Black";
+    exhaustButton.innerText = "💢🔟0️⃣0️⃣0️⃣";
+    exhaustButtonH.innerText = "💢 5️⃣0️⃣0️⃣0️⃣";
+    resetBtnAnswer.style.display = "none";
+    answerInput.style.display = "none"   
+    answerCheck.innerHTML = " 🆗 ❌ 🆕 🎮 🆗 ❌  🆕 🎮 🆗 ❌ 🆕 🎮 🆗  ❌  🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌  🆗  ❌  🆕 🎮 🆗   ❌  🆕 🎮 🆗 ❌ 🆕 🎮 🆗" + 
+    "❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗 ❌ 🆕 🎮 🆗  ❌  🆕 🎮 🆗  ❌ 🆕 🎮 🆗 ❌ 🎮 "
+    + score;
+    score = 0;
+    }
+}
+
+getData();
+    exhaustButton.innerText = "💢 🔟0️⃣0️⃣0️⃣";
+    exhaustButtonH.innerText = "💢  5️⃣0️⃣0️⃣0️⃣";
+    answerCheck.innerHTML = "";
+    resetBtnAnswer.style.display = "block";
+    answerInput.style.display = "block";
+    greeted.style.display = "none";
+}
+
+function displayBtn() {
+    resetBtnAnswer.style.display = "none";
+    answerInput.style.display = "none";
+    greeted.innerHTML = "❗JEOPARDY ";
+}
+displayBtn();
+
+function endGame(){
+
+        score = 0;
+        exhaustButton.innerText = "💢 RESTARTREST";
+        exhaustButtonH.innerText = "💢 RESTARTREST";
+        answerInput.value = "";
+        questionText.innerHTML = "";
+        matchContainer.style.background ="Black";
+        scoreMark.innerHTML = " 🥇" + score;
+        answerCheck.innerHTML = "ANSWER ME ANSWER ME ANSWER ME ANSWER ME ANSWER ME ANSWER ME ANS";
+        setTimeout(function removeMessage() {
+        answerCheck.innerHTML = " ";
+        }, 2000)
+
+}
+
